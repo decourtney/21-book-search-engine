b02fabd716595792c46b10d5d0c253ab18c0a8fa
decourtney <donovan.courtney@gmail.com>
2023-04-12 11:20:07 -0700

    somewhat functional

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,43 +1,49 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
+import { useMutation } from '@apollo/client'
+import { ADD_USER } from '../utils/mutations'
 import Auth from '../utils/auth';
+import { createUser } from '../utils/API';
 
-const SignupForm = () => {
+
+const SignupForm = () =>
+{
   // set initial form state
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
   // set state for form validation
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+  const [addUser, { error, data }] = useMutation(ADD_USER);
 
-  const handleInputChange = (event) => {
+  const handleInputChange = (event) =>
+  {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
   };
 
-  const handleFormSubmit = async (event) => {
+  const handleFormSubmit = async (event) =>
+  {
     event.preventDefault();
 
     // check if form has everything (as per react-bootstrap docs)
     const form = event.currentTarget;
-    if (form.checkValidity() === false) {
+    if (form.checkValidity() === false)
+    {
       event.preventDefault();
       event.stopPropagation();
     }
 
-    try {
-      const response = await createUser(userFormData);
+    try
+    {
+      const { data } = await addUser({
+        variables: { ...userFormData }
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
+      Auth.login(data.addUser.token);
+    } catch (err)
+    {
       console.error(err);
       setShowAlert(true);
     }
--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import {
+import
+{
   Container,
   Col,
   Form,
@@ -8,11 +9,15 @@ import {
   Row
 } from 'react-bootstrap';
 
+import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { saveBook, searchGoogleBooks } from '../utils/API';
 
-const SearchBooks = () => {
+
+const SearchBooks = () =>
+{
   // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
   // create state for holding our search field data
@@ -23,22 +28,27 @@ const SearchBooks = () => {
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
-  useEffect(() => {
+  useEffect(() =>
+  {
     return () => saveBookIds(savedBookIds);
   });
 
   // create method to search for books and set state on form submit
-  const handleFormSubmit = async (event) => {
+  const handleFormSubmit = async (event) =>
+  {
     event.preventDefault();
 
-    if (!searchInput) {
+    if (!searchInput)
+    {
       return false;
     }
 
-    try {
+    try
+    {
       const response = await searchGoogleBooks(searchInput);
 
-      if (!response.ok) {
+      if (!response.ok)
+      {
         throw new Error('something went wrong!');
       }
 
@@ -54,33 +64,35 @@ const SearchBooks = () => {
 
       setSearchedBooks(bookData);
       setSearchInput('');
-    } catch (err) {
+    } catch (err)
+    {
       console.error(err);
     }
   };
 
   // create function to handle saving a book to our database
-  const handleSaveBook = async (bookId) => {
+  const handleSaveBook = async (bookId) =>
+  {
     // find the book in `searchedBooks` state by the matching id
-    const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
+    const { bookToSave } = searchedBooks.find((book) => book.bookId === bookId);
+    const [saveBook, { error }] = useMutation(SAVE_BOOK);
 
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-    if (!token) {
+    if (!token)
+    {
       return false;
     }
 
-    try {
-      const response = await saveBook(bookToSave, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+    try
+    {
+      const { data } = await saveBook({ variables: { ...bookToSave } });
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
-    } catch (err) {
+    } catch (err)
+    {
       console.error(err);
     }
   };
@@ -119,7 +131,8 @@ const SearchBooks = () => {
             : 'Search for a book to begin'}
         </h2>
         <Row>
-          {searchedBooks.map((book) => {
+          {searchedBooks.map((book) =>
+          {
             return (
               <Col md="4">
                 <Card key={book.bookId} border='dark'>
