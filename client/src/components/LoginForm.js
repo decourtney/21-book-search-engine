5791563c5aec75715cc562f1420f282709571a4c
decourtney <53956789+decourtney@users.noreply.github.com>
2023-04-11 22:57:55 -0500

    signupform conversion

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,9 +1,11 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { loginUser } from '../utils/API';
+import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../utils/mutations";
 import Auth from '../utils/auth';
+// import { loginUser } from "../utils/API";
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
